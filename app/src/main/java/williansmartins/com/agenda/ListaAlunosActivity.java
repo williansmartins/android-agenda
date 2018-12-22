488abf8c10e64a853e9901efe4c7c41a1f286554
e-wmorais <willians.morais.e@riachuelo.com.br>
2018-12-21 23:20:29 -0200

    criando lista de alunos

---
+++ app/src/main/java/williansmartins/com/agenda/ListaAlunosActivity.java
@@ -0,0 +1,22 @@
+package williansmartins.com.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Willians", "Nayara", "Felipe", "Miguel"};
+
+        ListView listaDeAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaDeAlunos.setAdapter(adapter);
+    }
+}
