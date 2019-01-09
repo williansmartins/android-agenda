559ac76907ff7c03b0479a2945cdc1dacc31fe91
Willians Martins <willians.morais.e@riachuelo.com.br>
2019-01-09 17:09:12 -0200

    add botao e toast - gacp2

---
+++ app/src/main/java/williansmartins/com/agenda/FormularioActivity.java
@@ -0,0 +1,24 @@
+package williansmartins.com.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+        Button salvar = findViewById(R.id.formulario_salvar);
+
+        salvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioActivity.this, "clicou:" + R.id.formulario_nome, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
